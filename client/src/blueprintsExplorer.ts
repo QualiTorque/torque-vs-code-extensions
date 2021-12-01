60948116285ed19e2ce23e59e1b5e102e9872438
kalsky <yanivka@gmail.com>
2021-12-01 13:52:29 -0600

    some lint fixes

--- client/src/blueprintsExplorer.ts
+++ client/src/blueprintsExplorer.ts
@@ -1,129 +1,84 @@
-import * as vscode from 'vscode';
-import { Blueprint } from './models';
-import { ProfilesManager } from './profilesManager';
+import * as vscode from 'vscode'
+import { Blueprint } from './models'
+import { ProfilesManager } from './profilesManager'
 
 export class BlueprintsProvider implements vscode.TreeDataProvider<Blueprint> {
+    private _onDidChangeTreeData: vscode.EventEmitter<Blueprint | undefined | void> = new vscode.EventEmitter<Blueprint | undefined | void>()
+    readonly onDidChangeTreeData: vscode.Event<Blueprint | undefined | void> = this._onDidChangeTreeData.event
+    private firstLoad = true
+    
+    refresh(): void {
+        this._onDidChangeTreeData.fire()
+    }
 
-	private _onDidChangeTreeData: vscode.EventEmitter<Blueprint | undefined | void> = new vscode.EventEmitter<Blueprint | undefined | void>();
-	readonly onDidChangeTreeData: vscode.Event<Blueprint | undefined | void> = this._onDidChangeTreeData.event;
-    //connections: Array<object>;
-	private firstLoad = true;
+    getTreeItem(element: Blueprint): vscode.TreeItem {
+        return element
+    }
 
-	constructor() {}
+    getChildren(element?: Blueprint): Thenable<Blueprint[]> {
+        return new Promise(async (resolve) => {
+            if (!element && this.firstLoad) {
+                this.firstLoad = false
+                return resolve([])
+            }
 
-	refresh(): void {
-		this._onDidChangeTreeData.fire();
-	}
+            if (element) {
+                return resolve([])
+            } else {
+                const activeProfile = (ProfilesManager.getInstance().getActive() === undefined) ? '' : ProfilesManager.getInstance().getActive().label
+                const results = []
 
-	getTreeItem(element: Blueprint): vscode.TreeItem {
-		return element;
-	}
+                if (activeProfile === '') {
+                    vscode.window.showInformationMessage('No default profile is defined');
+                    results.push(this.getLoginTreeItem())
+                    return resolve(results)
+                }
+                else {
+                    var bps = []
 
-	getChildren(element?: Blueprint): Thenable<Blueprint[]> {
-		return new Promise(async (resolve) => {
-			if (!element && this.firstLoad) {
-				this.firstLoad = false;
-				return resolve([])
-			}
-			
-			if (element) {
-				return resolve([]);
-			} else {
-				const active_profile = (ProfilesManager.getInstance().getActive() === undefined) ? "" : ProfilesManager.getInstance().getActive().label
-				const results = []
+                    vscode.commands.executeCommand('list_blueprints')
+                    .then(async (result:string) => {
+                        if (result.length > 0) {
+                            const blueprintsJson = JSON.parse(result)
 
-				if (active_profile === "") {
-					vscode.window.showInformationMessage('No default profile is defined');
-					results.push(this.getLoginTreeItem())
-					return resolve(results);
-				}
-				else { 
-					var bps = [];
-		
-					vscode.commands.executeCommand('list_blueprints')
-					.then(async (result:string) => {
-						if (result.length > 0) {
-							const blueprintsJson = JSON.parse(result);
+                            const toBp = (blueprintName: string, description: string, isSample: boolean, inputs: Array<string>, artifacts: string, branch: string):
+                            Blueprint => {
+                                let cleanName = blueprintName
+                                if (isSample) {
+                                    cleanName = cleanName.replace('[Sample]', '')
+                                }
+                                return new Blueprint(cleanName, description, vscode.TreeItemCollapsibleState.None, {
+                                    command: 'extension.openReserveForm',
+                                    title: '',
+                                    arguments: [blueprintName, inputs, artifacts, branch]
+                                })
+                            };
 
-							const toBp = (blueprintName: string, description: string, is_sample: boolean, inputs: Array<string>, artifacts: string, branch: string):
-							Blueprint => {
-								let cleanName = blueprintName;
-								if (is_sample)
-									cleanName = cleanName.replace('[Sample]', '')
-								return new Blueprint(cleanName, description, vscode.TreeItemCollapsibleState.None, {
-									command: 'extension.openReserveForm',
-									title: '',
-									arguments: [blueprintName, inputs, artifacts, branch]
-								});
-							};
+                            for (let b = 0; b < blueprintsJson.length; b++) {
+                                const bpj = blueprintsJson[b]
+                                if (bpj.errors.length === 0 && bpj.enabled) {
+                                    const re = new RegExp('(?<=blob\/)(.*)(?=\/blueprints)')
+                                    var branch
+                                    if (bpj.url) {
+                                        branch = bpj.url.match(re)[0];
+                                        const bp = toBp(bpj.blueprint_name, bpj.description, bpj.is_sample, bpj.inputs, bpj.artifacts, branch)
+                                        bps.push(bp)
+                                    }
+                                }
+                            }
+                            return resolve(bps)
+                        }
+                        else return resolve([])
+                    })
+                }
+            }
+        })
+    }
 
-							for (let b=0; b<blueprintsJson.length; b++) {
-								const bpj = blueprintsJson[b];
-								if (bpj.errors.length==0 && bpj.enabled) {
-									let re = new RegExp('(?<=blob\/)(.*)(?=\/blueprints)');
-									var branch;
-									if (bpj.url) {
-										branch = bpj.url.match(re)[0];
-										const bp = toBp(bpj.blueprint_name, bpj.description, bpj.is_sample, bpj.inputs, bpj.artifacts, branch);
-										bps.push(bp);
-									}
-								}
-							}
-							return resolve(bps);
-						}
-						else return resolve([])
-					})
-					//return resolve(bps);
-					//return resolve(this.getOnlineBlueprints());
-				}
-					
-			}
-		});
-	}
-
-	private getLoginTreeItem() : vscode.TreeItem {
-		const message = new vscode.TreeItem("Login to Torque", vscode.TreeItemCollapsibleState.None)
-		message.command = {command: 'profilesView.addProfile', 'title': 'Login'}
-		message.tooltip = "Currently you don't have any profiles configured. Login to Torque in order to create the first profile"
-		return message
-	}
-
-	/**
-	 * Given the path to package.json, read all its dependencies and devDependencies.
-	 */
-	private getOnlineBlueprints(): Blueprint[] {
-		var bps = [];
-		
-		vscode.commands.executeCommand('list_blueprints')
-		.then(async (result:string) => {
-			if (result.length > 0) {
-				const blueprintsJson = JSON.parse(result);
-
-				const toBp = (blueprintName: string, description: string, is_sample: boolean, inputs: Array<string>, artifacts: string, branch: string):
-				Blueprint => {
-					let cleanName = blueprintName;
-					if (is_sample)
-						cleanName = cleanName.replace('[Sample]', '')
-					return new Blueprint(cleanName, description, vscode.TreeItemCollapsibleState.None, {
-						command: 'extension.openReserveForm',
-						title: '',
-						arguments: [blueprintName, inputs, artifacts, branch]
-					});
-				};
-
-				for (let b=0; b<blueprintsJson.length; b++) {
-					const bpj = blueprintsJson[b];
-					if (bpj.errors.length==0 && bpj.enabled) {
-						let re = new RegExp('(?<=blob\/)(.*)(?=\/blueprints)');
-						const branch = bpj.url.match(re)[0]
-
-						const bp = toBp(bpj.blueprint_name, bpj.description, bpj.is_sample, bpj.inputs, bpj.artifacts, branch);
-						bps.push(bp);
-					}
-				}
-			}
-			return bps;	
-		})
-		return bps;
-	}
+    private getLoginTreeItem() : vscode.TreeItem {
+        const message = new vscode.TreeItem('Login to Torque', vscode.TreeItemCollapsibleState.None)
+        message.command = { command: 'profilesView.addProfile', title: 'Login' }
+        message.tooltip = "Currently you don't have any profiles configured. Login to Torque in order to create the first profile"
+        return message
+    }
 }
--- client/src/setup.ts
+++ client/src/setup.ts
@@ -1,99 +1,92 @@
-import { existsSync } from "fs";
-import { join } from "path";
-import { ExtensionContext, ProgressLocation, window, workspace } from "vscode";
-import { IS_WIN, LS_VENV_NAME, LS_VENV_PATH, TORQUE_LS_SERVER } from "./constants";
-import { execAsync, readdirAsync } from "./utils";
+import { existsSync } from 'fs'
+import { join } from 'path'
+import { ExtensionContext, ProgressLocation, window, workspace } from 'vscode'
+import { IS_WIN, LS_VENV_NAME, LS_VENV_PATH } from './constants'
+import { execAsync } from './utils'
 
 async function checkPythonVersion(python: string): Promise<boolean> {
   try {
-    const [major, minor] = await getPythonVersion(python);
-    return major === 3 && minor > 5;
+    const [major, minor] = await getPythonVersion(python)
+    return major === 3 && minor > 5
   } catch {
-    return false;
+    return false
   }
 }
 
 async function createVirtualEnvironment(python: string, name: string, cwd: string): Promise<string> {
-  const path = join(cwd, name);
+  const path = join(cwd, name)
   if (!existsSync(path)) {
-    if (IS_WIN && !python.startsWith('"'))
-        python = '"' + python + '"';
-    const createVenvCmd = `${python} -m venv ${name}`;
-    await execAsync(createVenvCmd, { cwd });
+    if (IS_WIN && !python.startsWith('"')) {
+      python = '"' + python + '"'
+    }
+    const createVenvCmd = `${python} -m venv ${name}`
+    await execAsync(createVenvCmd, { cwd })
   }
-  return path;
+  return path
 }
 
 export async function getPython(): Promise<string> {
-  let python = workspace.getConfiguration("python").get<string>("pythonPath", getPythonCrossPlatform());
+  let python = workspace.getConfiguration('python').get<string>('pythonPath', getPythonCrossPlatform())
   if (await checkPythonVersion(python)) {
-    return python;
+    return python
   }
 
   python = await window.showInputBox({
     ignoreFocusOut: true,
-    placeHolder: "Enter a path to the python 3.6+.",
-    prompt: "This python will be used to create a virtual environment inside the extension directory.",
+    placeHolder: 'Enter a path to a python v3.6+.',
+    prompt: 'This python will be used to create a virtual environment inside the extension directory.',
     validateInput: async (value: string) => {
       if (await checkPythonVersion(value)) {
-        return null;
+        return null
       } else {
-        return "Not a valid python path!";
+        return 'Not a valid python path!'
       }
     },
-  });
+  })
 
   // User canceled the input
-  if (python === "undefined") {
-    throw new Error("Python 3.6+ is required!");
+  if (python === 'undefined') {
+    throw new Error('Python 3.6+ is required!')
   }
 
-  if (IS_WIN)
-    python = '"' + python + '"';
-  return python;
+  if (IS_WIN) {
+    python = '"' + python + '"'
+  }
+  return python
 }
 
 function getPythonCrossPlatform(): string {
-  return IS_WIN ? "python.exe" : "python3";
+  return IS_WIN ? 'python.exe' : 'python3'
 }
 
 export function getPythonFromVenvPath(venvPath: string = LS_VENV_PATH): string {
-  return IS_WIN ? join(venvPath, "Scripts", "python.exe") : join(venvPath, "bin", "python");
+  return IS_WIN ? join(venvPath, 'Scripts', 'python.exe') : join(venvPath, 'bin', 'python')
 }
 
 async function getPythonVersion(python: string): Promise<number[]> {
-    if (IS_WIN && !python.startsWith('"'))
-        python = `"${python}"`;
-  const getPythonVersionCmd = `${python} --version`;
-  const version = await execAsync(getPythonVersionCmd);
-  return version.match(new RegExp(/\d+/g)).map((v) => Number.parseInt(v));
+  if (IS_WIN && !python.startsWith('"')) {
+    python = `"${python}"`
+  }
+  const getPythonVersionCmd = `${python} --version`
+  const version = await execAsync(getPythonVersionCmd)
+  return version.match(new RegExp(/\d+/g)).map((v) => Number.parseInt(v))
 }
-
-// async function getVenvPackageVersion(python: string, name: string): Promise<number[] | null> {
-//   const listPipPackagesCmd = `${python} -m pip show ${name}`;
-//   try {
-//     const packageInfo = await execAsync(listPipPackagesCmd);
-//     const packageVersion = packageInfo.match(new RegExp(/Version: \d\.\d\.\d/g));
-//     return packageVersion.pop().match(new RegExp(/\d/g)).map((v) => Number.parseInt(v));
-//   } catch (err) {
-//     return null;
-//   }
-// }
 
 async function installRequirements(python: string, cwd: string) {
   if (existsSync(cwd)) {
-    if (IS_WIN && !python.startsWith('"'))
-        python = `"${python}"`;
-    await execAsync(`${python} -m pip install --upgrade --force-reinstall -r requirements.txt --log torque-extension-req-install.log`, { cwd });
+    if (IS_WIN && !python.startsWith('"')) {
+      python = `"${python}"`
+    }
+    await execAsync(`${python} -m pip install --upgrade --force-reinstall -r requirements.txt --log torque-extension-req-install.log`, { cwd })
   }
 }
 
 export async function installLSWithProgress(context: ExtensionContext): Promise<string> {
   // Check if LS is already installed
-  let venvPython = getPythonFromVenvPath();
+  let venvPython = getPythonFromVenvPath()
   if (existsSync(venvPython))
   {
-    return Promise.resolve(venvPython);
+    return Promise.resolve(venvPython)
   }
   
   // Install with progress bar
@@ -102,25 +95,25 @@ export async function installLSWithProgress(context: ExtensionContext): Promise<
   }, (progress): Promise<string> => {
     return new Promise<string>(async (resolve, reject) => {
       try {
-        progress.report({ message: "First-time Torque extension initialization" });
+        progress.report({ message: 'First-time Torque extension initialization' })
 
         // Get python interpreter
-        const python = await getPython();
+        const python = await getPython()
 
         // Create virtual environment
-        const venv = await createVirtualEnvironment(python, LS_VENV_NAME, context.extensionPath);
+        const venv = await createVirtualEnvironment(python, LS_VENV_NAME, context.extensionPath)
 
         // Install source from wheels
         venvPython = getPythonFromVenvPath(venv);
-        const requirementsPath = join(context.extensionPath, "out", "server");
-        await installRequirements(venvPython, requirementsPath);
+        const requirementsPath = join(context.extensionPath, 'out', 'server')
+        await installRequirements(venvPython, requirementsPath)
 
-        window.showInformationMessage("Torque extension is ready! :)");
-        resolve(venvPython);
+        window.showInformationMessage('Torque extension is ready! 🎉')
+        resolve(venvPython)
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
-  });
+    })
+  })
 
 }
