567547c98c9276363e50c85aef80f8d217c81736
Dmytro Dovbii <dmytro.d@quali.com>
2022-03-04 18:37:20 +0200

    add unit test

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -6,6 +6,9 @@ from pygls.lsp.types.basic_structures import Position, Range
 from server.ats.trees.blueprint import (
     ApplicationNode,
     ApplicationResourceNode,
+    BlueprintFullInputNode,
+    BlueprintInputNode,
+    BlueprintInputsSequence,
     BlueprintTree,
     ServiceNode,
     ServiceResourceNode,
@@ -105,7 +108,7 @@ class TestValidationHandler(unittest.TestCase):
                             ),
                         ),
                     ]
-                )
+                ),
             ),
         )
         validator = ValidationHandler(tree, self.test_doc)
@@ -126,12 +129,8 @@ class TestValidationHandler(unittest.TestCase):
                 key=ScalarNode(_text="inputs_node"),
                 value=ScalarNodesSequence(
                     nodes=[
-                        ScalarNode(
-                            start_pos=(5, 2), end_pos=(5, 6), _text=word
-                        ),
-                        ScalarNode(
-                            start_pos=(6, 2), end_pos=(6, 6), _text=word
-                        ),
+                        ScalarNode(start_pos=(5, 2), end_pos=(5, 6), _text=word),
+                        ScalarNode(start_pos=(6, 2), end_pos=(6, 6), _text=word),
                     ]
                 ),
             )
@@ -144,51 +143,135 @@ class TestValidationHandler(unittest.TestCase):
         for d in diags:
             self.assertEqual(
                 d.message,
-                f"Multiple declarations of output '{word}'. Outputs are not case sensitive."
+                f"Multiple declarations of output '{word}'. Outputs are not case sensitive.",
             )
         self.assertEqual(diags[0].range, self._get_range((5, 2), (5, 6)))
         self.assertEqual(diags[1].range, self._get_range((6, 2), (6, 6)))
 
 
 class TestBlueprintValidationHandler(TestValidationHandler):
+    def test_validate_default_value_not_in_possible_values_list(self):
+        wrong_value = "ba"
+
+        tree = BlueprintTree(
+            inputs_node=PropertyNode(
+                key=ScalarNode(_text="inputs_node"),
+                value=BlueprintInputsSequence(
+                    nodes=[
+                        BlueprintInputNode(
+                            key=ScalarNode(_text="ABC"),
+                            value=BlueprintFullInputNode(
+                                display_style=None,
+                                description=None,
+                                default_value=PropertyNode(
+                                    key=ScalarNode(_text="default_value"),
+                                    value=ScalarNode(
+                                        _text=f"{wrong_value}",
+                                        start_pos=(18, 21),
+                                        end_pos=(18, 23)
+                                    ),
+                                ),
+                                optional=None,
+                                possible_values=PropertyNode(
+                                    key=ScalarNode(_text="possible_values"),
+                                    value=ScalarNodesSequence(
+                                        nodes=[
+                                            ScalarNode(_text="a"),
+                                            ScalarNode(_text="b"),
+                                        ]
+                                    ),
+                                ),
+                            ),
+                        )
+                    ]
+                ),
+            ),
+        )
+        validator = BlueprintValidationHandler(tree, self.test_doc)
+        validator._validate_default_value_in_possible_values()
+        self.assertEqual(len(validator._diagnostics), 1)
+        d = validator._diagnostics[0]
+        self.assertEqual(f"Default value '{wrong_value}' must be in the list of possible values", d.message)
+
+        self.assertEqual(d.range, self._get_range((18, 21), (18, 23)))
+
+
     def test_validate_resources_have_inputs(self):
         tree = BlueprintTree(
             inputs_node=None,
             applications=PropertyNode(
-                key=ScalarNode(_text="applications"), value=BlueprintTree.AppsSequence(
-                    nodes=[ApplicationNode(
-                        key=ScalarNode(_text="azure-ubuntu"),
-                        value=ApplicationResourceNode(
-                            input_values=PropertyNode(
-                                key=ScalarNode(_text='input_values'),
-                                value=TextMappingSequence(
-                                    nodes=[
-                                        TextMapping(
-                                            key=ScalarNode(start_pos=(7, 10), end_pos=(7, 23), _text='instance_type'),
-                                            value=None)])))
-                    )])),
+                key=ScalarNode(_text="applications"),
+                value=BlueprintTree.AppsSequence(
+                    nodes=[
+                        ApplicationNode(
+                            key=ScalarNode(_text="azure-ubuntu"),
+                            value=ApplicationResourceNode(
+                                input_values=PropertyNode(
+                                    key=ScalarNode(_text="input_values"),
+                                    value=TextMappingSequence(
+                                        nodes=[
+                                            TextMapping(
+                                                key=ScalarNode(
+                                                    start_pos=(7, 10),
+                                                    end_pos=(7, 23),
+                                                    _text="instance_type",
+                                                ),
+                                                value=None,
+                                            )
+                                        ]
+                                    ),
+                                )
+                            ),
+                        )
+                    ]
+                ),
+            ),
             services=PropertyNode(
                 key=ScalarNode(_text="services"),
                 value=BlueprintTree.ServicesSequence(
-                    nodes=[ServiceNode(
-                        key=ScalarNode(_text="sleep",),
-                        value=ServiceResourceNode(
-                            input_values=PropertyNode(
-                                key=ScalarNode(_text='input_values'),
-                                value=TextMappingSequence(
-                                    nodes=[
-                                        TextMapping(
-                                            key=ScalarNode(start_pos=(9, 10), end_pos=(9, 15), _text='seconds'),
-                                            value=None)]))
-                        ))])))
+                    nodes=[
+                        ServiceNode(
+                            key=ScalarNode(
+                                _text="sleep",
+                            ),
+                            value=ServiceResourceNode(
+                                input_values=PropertyNode(
+                                    key=ScalarNode(_text="input_values"),
+                                    value=TextMappingSequence(
+                                        nodes=[
+                                            TextMapping(
+                                                key=ScalarNode(
+                                                    start_pos=(9, 10),
+                                                    end_pos=(9, 15),
+                                                    _text="seconds",
+                                                ),
+                                                value=None,
+                                            )
+                                        ]
+                                    ),
+                                )
+                            ),
+                        )
+                    ]
+                ),
+            ),
+        )
         validator = BlueprintValidationHandler(tree, self.test_doc)
         validator._validate_blueprint_resources_have_input_values()
 
         self.assertEqual(len(validator._diagnostics), 2)
         if validator._diagnostics:
             for d in validator._diagnostics:
-                self.assertTrue(d.message.endswith("input must have a value or a blueprint input "
-                                                   "with the same name should be defined"))
-                self.assertTrue(d.range in [self._get_range((7, 10), (7, 23)),
-                                            self._get_range((9, 10), (9, 15))]
-                                )
+                self.assertTrue(
+                    d.message.endswith(
+                        "input must have a value or a blueprint input "
+                        "with the same name should be defined"
+                    )
+                )
+                self.assertTrue(
+                    d.range
+                    in [
+                        self._get_range((7, 10), (7, 23)),
+                        self._get_range((9, 10), (9, 15)),
+                    ]
+                )
