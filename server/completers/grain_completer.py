a74f70eb9c878fce64ba872063439adefe7551c5
Dmytro Dovbii <dmytro.d@quali.com>
2022-05-13 15:25:21 +0300

    rework depend_on completion

--- server/completers/grain_completer.py
+++ server/completers/grain_completer.py
@@ -1,12 +1,9 @@
-from re import X
 from typing import List
 
 from pygls.lsp.types.language_features.completion import (
     CompletionItem,
     CompletionItemKind
 )
-from pygls.workspace import Workspace
-from pyparsing import PrecededBy
 
 from server.ats.trees.blueprint_v2 import BlueprintV2Tree, GrainNode
 from server.ats.trees.common import PropertyNode, YamlNode
@@ -16,28 +13,30 @@ from server.completers.base import Completer
 
 class GrainObjectCompleter(Completer):
     def get_completions(self) -> List[CompletionItem]:
-        proccessing_funcs = {
+        processing_funcs = {
             "depends_on": self._process_depends_on
         }
-        property = self._find_nearest_prop()
-        func = proccessing_funcs.get(property.identifier, None)
+        n_property = self._find_nearest_prop()
+        func = processing_funcs.get(property.identifier, None)
 
         if func is None:
             return []
 
-        return func(property)
+        return func(n_property)
 
     def _process_depends_on(self, property_node: PropertyNode):
         completions_items = []
 
         cur_grain_name = self._get_grain_name()
         grains_names_list = self.tree.get_grains_names()
-        typed = "" if property_node.value is None else property_node.value.text
+        deps_text = "" if property_node.value is None else property_node.value.text
+        deps = deps_text.split(",")
+        typed = deps[-1].strip()
 
         for name in grains_names_list:
             if name in [cur_grain_name, typed]:
                 continue
-            if name.startswith(typed):
+            if name.startswith(typed) and name not in deps:
                 completions_items.append(CompletionItem(label=name, kind=CompletionItemKind.Reference))
 
         return completions_items
@@ -55,6 +54,3 @@ class GrainObjectCompleter(Completer):
                 return None
         
         return None
-
-
-       
